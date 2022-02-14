5d2e50e1897045c6d7035f8d1fe782aa7eef2197
jetlye2804 <82960491+jetlye2804@users.noreply.github.com>
2022-02-14 16:26:47 +0800

    Add files via upload

---
+++ src/bot/bot.module.ts
@@ -0,0 +1,32 @@
+import { DiscordModule } from '@discord-nestjs/core';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Intents, Message } from 'discord.js';
+import { BotGateway } from './bot.gateway';
+
+@Module({
+    imports: [
+        DiscordModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService: ConfigService) => ({
+                token: 'OTExOTE1NDkyODc5NTIzODQy.YZoVfA.ftxUNi3dMNnKMva4qA8oRq0PQ9w',
+                commands: ['play.command.js', 'playlist.command.js'],
+                discordClientOptions: {
+                    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+                },
+                removeGlobalCommands: true,
+                registerCommandOptions: [
+                    {
+                        forGuild: '911603487610109973',
+                        allowFactory: (message: Message) =>
+                            !message.author.bot && message.content === '!deploy',
+                        removeCommandsBefore: true,
+                    },
+                ],
+            }),
+            inject: [ConfigService],
+        }),
+    ],
+    providers: [BotGateway],
+})
+export class BotModule { }
